5ed86128b1d28877b38ebc0a22f62ee1c9baf66f
Anjan Roy <hello@itzmeanjan.in>
2024-02-19 19:51:07 +0400

    add tests ensuring functional correctness of modulo (Zq) arithmetic
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

---
+++ tests/test_field.cpp
@@ -0,0 +1,51 @@
+#include "field.hpp"
+#include <gtest/gtest.h>
+
+TEST(RaccoonSign, ArithmeticOverZq)
+{
+  constexpr size_t itr_cnt = 1ul << 20;
+  constexpr size_t exp = 1ul << 8;
+
+  prng::prng_t prng;
+
+  for (size_t i = 0; i < itr_cnt; i++) {
+    const auto a = field::zq_t::random(prng);
+    const auto b = field::zq_t::random(prng);
+
+    // Addition, Subtraction and Negation
+    const auto c = a + b;
+    const auto d = c - b;
+    const auto e = c - a;
+
+    EXPECT_EQ(d, a);
+    EXPECT_EQ(e, b);
+
+    // Multiplication, Inversion and Division
+    const auto f = a * b;
+    const auto g = f / b;
+    const auto h = f / a;
+
+    if (b != field::zq_t::zero()) {
+      EXPECT_EQ(g, a);
+    } else {
+      EXPECT_EQ(g, field::zq_t::zero());
+    }
+
+    if (a != field::zq_t::zero()) {
+      EXPECT_EQ(h, b);
+    } else {
+      EXPECT_EQ(h, field::zq_t::zero());
+    }
+
+    // Exponentiation
+    const auto j = std::max(a, b);
+    const auto k = j ^ exp;
+
+    auto l = field::zq_t::one();
+    for (size_t i = 0; i < exp; i++) {
+      l *= j;
+    }
+
+    EXPECT_EQ(k, l);
+  }
+}
