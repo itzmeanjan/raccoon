994f39fd82cf1e11fb0f48d8fd3fa9ce0bc0a839
Anjan Roy <hello@itzmeanjan.in>
2024-02-26 13:39:00 +0530

    implement `expandA` function for generating public matrix A
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/sampling.hpp
+++ include/sampling.hpp
@@ -8,7 +8,9 @@
 
 namespace sampling {
 
-// Given a 64 -bit header and `𝜅` -bits seed as input, this routine can be used for mapping them to a degree n-1 polynomial f.
+// Given a 64 -bit header and `𝜅` -bits seed as input, this routine is used for mapping them to a degree n-1 polynomial f, following algorithm 5 of
+// https://raccoonfamily.org/wp-content/uploads/2023/07/raccoon.pdf.
+//
 // This routine is invoked when expanding seed for computing matrix A.
 template<size_t n, size_t 𝜅>
 static inline void
@@ -33,6 +35,26 @@ sampleQ(std::span<const uint8_t, 8> hdr, std::span<const uint8_t, 𝜅 / 8> 𝜎
     } while (f_i >= field::Q);
 
     f[i] = f_i;
+  }
+}
+
+// Given `𝜅` -bits seed as input, this routine is used for generating public matrix A, following algorithm 6 of
+// https://raccoonfamily.org/wp-content/uploads/2023/07/raccoon.pdf.
+template<size_t k, size_t l, size_t n, size_t 𝜅>
+static inline void
+expandA(std::span<const uint8_t, 𝜅 / 8> seed, std::span<field::zq_t, k * l * n> A)
+{
+  for (size_t i = 0; i < k; i++) {
+    for (size_t j = 0; j < l; j++) {
+
+      uint64_t hdr = 0;
+      hdr |= (static_cast<uint64_t>(j) << 16) | (static_cast<uint64_t>(i) << 8) | (static_cast<uint64_t>('A') << 0);
+
+      const size_t poly_off = (i * l + j) * n;
+      auto poly = std::span<field::zq_t, n>(A.subspan(poly_off, n));
+
+      sampleQ<n, 𝜅>(std::span<const uint8_t, sizeof(hdr)>(reinterpret_cast<uint8_t*>(hdr), sizeof(hdr)), seed, poly);
+    }
   }
 }
 
