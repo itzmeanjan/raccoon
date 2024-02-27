34e8b16d59061dd5c48e93f9d2d6a0b95fe3e628
Anjan Roy <hello@itzmeanjan.in>
2024-02-27 13:18:54 +0530

    initialize Ascon MRNG
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

---
+++ include/mrng.hpp
@@ -0,0 +1,67 @@
+#pragma once
+#include "ascon/aead/ascon80pq.hpp"
+#include "ascon/ascon_perm.hpp"
+#include "ascon/utils.hpp"
+#include "utils.hpp"
+#include <algorithm>
+#include <array>
+#include <numeric>
+
+// Masked Random Number Generator
+namespace mrng {
+
+// Masked Random Number Generator using Ascon80pq AEAD, following
+// https://github.com/masksign/raccoon/blob/e789b4b72a2b7e8a2205df49c487736985fc8417/ref-c/mask_random.c#L13-L187
+template<size_t d>
+  requires(raccoon_utils::is_power_of_2(d))
+struct mrng_t
+{
+private:
+  std::array<ascon_perm::ascon_perm_t, d - 1> state;
+
+public:
+  // Creates a Masked Random Number Generator instance, initializing (d-1) RNGs, ready to be squeezed.
+  //
+  // Following initialization function collects inspiration from
+  // https://github.com/masksign/raccoon/blob/e789b4b72a2b7e8a2205df49c487736985fc8417/ref-c/mask_random.c#L81-L124
+  inline mrng_t()
+  {
+    std::fill(this->state.begin(), this->state.end(), 0);
+
+    std::array<uint8_t, ascon80pq_aead::KEY_LEN> key;
+    std::array<uint8_t, ascon80pq_aead::NONCE_LEN> nonce;
+
+    auto _key = std::span(key);
+    auto _nonce = std::span(nonce);
+
+    std::iota(_key.begin(), _key.end(), 0);
+    std::iota(_nonce.begin(), _nonce.end(), 0);
+
+    const auto key0 = ascon_utils::from_be_bytes<uint64_t>(_key.template subspan<0, 8>());
+    const auto key1 = ascon_utils::from_be_bytes<uint64_t>(_key.template subspan<8, 8>());
+    const auto key2 = ascon_utils::from_be_bytes<uint32_t>(_key.template subspan<16, 4>());
+
+    const auto nonce0 = ascon_utils::from_be_bytes<uint64_t>(_nonce.subspan<0, 8>());
+    const auto nonce1 = ascon_utils::from_be_bytes<uint64_t>(_nonce.subspan<8, 8>());
+
+    for (size_t i = 0; i < d - 1; i++) {
+      auto i_share = this->state[i];
+
+      i_share[0] = (ascon80pq_aead::IV << 32) | (key0 >> 32);
+      i_share[1] = (key0 << 32) | (key1 >> 32);
+      i_share[2] = (key1 << 32) | static_cast<uint64_t>(key2);
+      i_share[3] = nonce0;
+      i_share[4] = nonce1;
+      i_share[3] += i; // Mutating nonce, using index of share 0 <= i < d
+
+      i_share.template permute<ascon_perm::MAX_ROUNDS>();
+
+      i_share[2] ^= (key0 >> 32);
+      i_share[3] ^= (key0 << 32) | (key1 >> 32);
+      i_share[4] ^= (key1 << 32) | static_cast<uint64_t>(key2);
+      i_share[4] ^= 1; // Domain separator
+    }
+  }
+};
+
+}
