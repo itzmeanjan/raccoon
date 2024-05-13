e515186abb7da844c12c30fae6623cf73aae1d75
Anjan Roy <hello@itzmeanjan.in>
2024-05-13 21:21:27 +0400

    refactor data type defining a matrix of polynomials over Zq
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/poly_mat.hpp
+++ include/poly_mat.hpp
@@ -1,76 +1,38 @@
 #pragma once
+#include "poly.hpp"
 #include "poly_vec.hpp"
-#include "polynomial.hpp"
-#include <array>
-#include <cstddef>
 
-namespace poly_mat {
+namespace raccoon_poly_mat {
 
-// A matrix of dimension `rows x cols` s.t. each of its elements are a degree 511 polynomial over Zq
+// A matrix of dimension `rows x cols` s.t. each of its elements are a degree 511 polynomial, defined over Zq
 template<size_t rows, size_t cols>
   requires((rows > 0) && (cols > 0))
 struct poly_mat_t
 {
 private:
-  std::array<polynomial::masked_poly_t<1>, rows * cols> elems{};
+  std::array<raccoon_poly::poly_t, rows * cols> elems{};
 
 public:
   // Constructor(s)
   inline constexpr poly_mat_t() = default;
 
-  // Access element at index (ridx, cidx) of the matrix s.t. `ridx < rows` and `cidx < cols`.
-  inline constexpr polynomial::masked_poly_t<1>& operator[](const std::pair<size_t, size_t> idx) { return this->elems[idx.first * cols + idx.second]; }
-  inline constexpr const polynomial::masked_poly_t<1>& operator[](const std::pair<size_t, size_t> idx) const
-  {
-    return this->elems[idx.first * cols + idx.second];
-  }
+  // Accessor(s) s.t. (row_idx, col_idx) | row_idx < rows && col_idx < cols
+  inline constexpr raccoon_poly::poly_t& operator[](const std::pair<size_t, size_t> idx) { return this->elems[idx.first * cols + idx.second]; }
+  inline constexpr const raccoon_poly::poly_t& operator[](const std::pair<size_t, size_t> idx) const { return this->elems[idx.first * cols + idx.second]; }
 
-  // Add two matrices of equal dimension
-  inline constexpr poly_mat_t operator+(const poly_mat_t<rows, cols>& rhs) const
-  {
-    poly_mat_t<rows, cols> res{};
-
-    for (size_t ridx = 0; ridx < rows; ridx++) {
-      for (size_t cidx = 0; cidx < cols; cidx++) {
-        res[{ ridx, cidx }] = (*this)[{ ridx, cidx }] + rhs[{ ridx, cidx }];
-      }
-    }
-
-    return res;
-  }
-
-  inline constexpr void operator+=(const poly_mat_t<rows, cols>& rhs) { *this = *this + rhs; }
-
-  // Subtract one matrix from another one s.t. both of them have equal dimension
-  inline constexpr poly_mat_t operator-(const poly_mat_t<rows, cols>& rhs) const
-  {
-    poly_mat_t<rows, cols> res{};
-
-    for (size_t ridx = 0; ridx < rows; ridx++) {
-      for (size_t cidx = 0; cidx < cols; cidx++) {
-        res[{ ridx, cidx }] = (*this)[{ ridx, cidx }] - rhs[{ ridx, cidx }];
-      }
-    }
-
-    return res;
-  }
-
-  inline constexpr poly_mat_t operator-=(const poly_mat_t<rows, cols>& rhs) { *this = *this - rhs; }
+  inline constexpr size_t num_rows() const { return rows; }
+  inline constexpr size_t num_cols() const { return cols; }
 
   // Multiply a matrix by another vector of compatible dimension, assuming both of them are in their NTT representation.
   template<size_t d>
-  inline constexpr poly_vec::poly_vec_t<rows, d> operator*(const poly_vec::poly_vec_t<cols, d>& rhs) const
+  inline constexpr raccoon_poly_vec::poly_vec_t<rows, d> operator*(const raccoon_poly_vec::poly_vec_t<cols, d>& rhs) const
   {
-    poly_vec::poly_vec_t<rows, d> res{};
+    raccoon_poly_vec::poly_vec_t<rows, d> res{};
 
-    for (size_t row_idx = 0; row_idx < rows; row_idx++) {
-      for (size_t col_idx = 0; col_idx < cols; col_idx++) {
+    for (size_t row_idx = 0; row_idx < this->num_rows(); row_idx++) {
+      for (size_t col_idx = 0; col_idx < this->num_cols(); col_idx++) {
         for (size_t shr_idx = 0; shr_idx < d; shr_idx++) {
-
-          // Multiply two equal degree polynomials in their NTT representation
-          for (size_t coeff_idx = 0; coeff_idx < polynomial::N; coeff_idx++) {
-            res[row_idx][{ shr_idx, coeff_idx }] += (*this)[{ row_idx, col_idx }][{ shr_idx, coeff_idx }] * rhs[col_idx][{ shr_idx, coeff_idx }];
-          }
+          res[row_idx][shr_idx] += (*this)[{ row_idx, col_idx }] * rhs[col_idx][shr_idx];
         }
       }
     }
@@ -81,7 +43,7 @@ public:
   // Given `𝜅` -bits seed as input, this routine is used for generating public matrix A, following algorithm 6 of
   // https://raccoonfamily.org/wp-content/uploads/2023/07/raccoon.pdf.
   template<size_t k, size_t l, size_t 𝜅>
-  static inline constexpr poly_mat_t<k, l> expandA(std::span<const uint8_t, 𝜅 / 8> seed)
+  static inline constexpr poly_mat_t<k, l> expandA(std::span<const uint8_t, 𝜅 / std::numeric_limits<uint8_t>::digits> seed)
   {
     poly_mat_t<k, l> A{};
 
