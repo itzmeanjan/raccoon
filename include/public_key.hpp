fdab963226c6e2e154f7ea765df90835cf0678e7
Anjan Roy <hello@itzmeanjan.in>
2024-05-14 09:28:40 +0400

    refactor public key struct to use new polynomial vector data type
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/public_key.hpp
+++ include/public_key.hpp
@@ -1,8 +1,6 @@
 #pragma once
 #include "poly_vec.hpp"
-#include "polynomial.hpp"
 #include "utils.hpp"
-#include <algorithm>
 
 namespace raccoon_pkey {
 
@@ -12,12 +10,12 @@ struct pkey_t
 {
 private:
   std::array<uint8_t, 𝜅 / std::numeric_limits<uint8_t>::digits> seed{};
-  poly_vec::poly_vec_t<k, 1> t{};
+  raccoon_poly_vec::poly_vec_t<k, 1> t{};
 
 public:
   // Constructor(s)
   inline constexpr pkey_t() = default;
-  inline constexpr pkey_t(std::span<const uint8_t, 𝜅 / std::numeric_limits<uint8_t>::digits> seed, const poly_vec::poly_vec_t<k, 1>& t)
+  inline constexpr pkey_t(std::span<const uint8_t, 𝜅 / std::numeric_limits<uint8_t>::digits> seed, const raccoon_poly_vec::poly_vec_t<k, 1>& t)
   {
     std::copy(seed.begin(), seed.end(), this->seed.begin());
     this->t = t;
@@ -26,10 +24,10 @@ public:
   // Accessor(s)
   inline constexpr std::span<const uint8_t, 𝜅 / std::numeric_limits<uint8_t>::digits> get_seed() const { return this->seed; }
   inline constexpr std::span<uint8_t, 𝜅 / std::numeric_limits<uint8_t>::digits> get_seed_mut() { return this->seed; }
-  inline constexpr const poly_vec::poly_vec_t<k, 1>& get_t_mut() const { return this->t; }
+  inline constexpr const raccoon_poly_vec::poly_vec_t<k, 1>& get_t_mut() const { return this->t; }
 
   // Byte serializes the public key.
-  inline constexpr void to_bytes(std::span<uint8_t, raccoon_utils::get_pkey_byte_len<𝜅, k, polynomial::N, 𝜈t>()> bytes) const
+  inline constexpr void to_bytes(std::span<uint8_t, raccoon_utils::get_pkey_byte_len<𝜅, k, raccoon_poly::N, 𝜈t>()> bytes) const
   {
     // Serialize `seed`
     std::copy_n(this->seed.begin(), this->seed.size(), bytes.begin());
@@ -50,7 +48,7 @@ public:
     static_assert(buf_max_sig_bytes <= sizeof(buffer), "Can't serialize public key into bytes using this method !");
 
     while (r_idx < k) {
-      for (size_t c_idx = 0; c_idx < polynomial::N; c_idx++) {
+      for (size_t c_idx = 0; c_idx < raccoon_poly::N; c_idx++) {
         if (buf_sig_bitcnt == buf_max_sig_bitcnt) {
           raccoon_utils::to_le_bytes(buffer, bytes.subspan(pkey_idx, buf_max_sig_bytes));
 
@@ -59,7 +57,7 @@ public:
           buffer = 0;
         }
 
-        buffer |= (this->t[r_idx][{ 0, c_idx }].raw() & coeff_sig_bitmask) << buf_sig_bitcnt;
+        buffer |= (this->t[r_idx][0][c_idx].raw() & coeff_sig_bitmask) << buf_sig_bitcnt;
         buf_sig_bitcnt += coeff_sig_bitcnt;
       }
 
@@ -76,7 +74,7 @@ public:
   }
 
   // Given a byte serialized public key, this routine helps in deserializing it, producing components (seed, t).
-  static inline constexpr pkey_t from_bytes(std::span<const uint8_t, raccoon_utils::get_pkey_byte_len<𝜅, k, polynomial::N, 𝜈t>()> bytes)
+  static inline constexpr pkey_t from_bytes(std::span<const uint8_t, raccoon_utils::get_pkey_byte_len<𝜅, k, raccoon_poly::N, 𝜈t>()> bytes)
   {
     pkey_t<𝜅, k, 𝜈t> pkey{};
 
@@ -101,10 +99,10 @@ public:
       pkey_idx += buf_max_sig_bytes;
 
       for (size_t i = 0; i < dec_coeffs_per_round; i++) {
-        const size_t ridx = t_idx >> polynomial::LOG2N;
-        const size_t cidx = t_idx & (polynomial::N - 1);
+        const size_t ridx = t_idx >> raccoon_poly::LOG2N;
+        const size_t cidx = t_idx & (raccoon_poly::N - 1);
 
-        pkey.t[ridx][{ 0, cidx }] = field::zq_t(buffer & coeff_sig_bitmask);
+        pkey.t[ridx][0][cidx] = field::zq_t(buffer & coeff_sig_bitmask);
         buffer >>= coeff_sig_bitcnt;
 
         t_idx++;
