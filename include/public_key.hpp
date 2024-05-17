4803ebaa89acc1b89e55d3bdef080e56736c2620
Anjan Roy <hello@itzmeanjan.in>
2024-05-17 20:35:52 +0400

    get rid of logic for public key component serialization and deserialization from struct managing public key interface
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/public_key.hpp
+++ include/public_key.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "poly_vec.hpp"
+#include "serialization.hpp"
 #include "utils.hpp"
 
 namespace raccoon_pkey {
@@ -29,89 +30,30 @@ public:
   // Returns byte length of the serialized public key.
   static inline constexpr size_t get_byte_len() { return raccoon_utils::get_pkey_byte_len<𝜅, k, raccoon_poly::N, 𝜈t>(); }
 
+  // [Constant-time] Checks for equality of two public keys.
+  inline constexpr bool operator==(const pkey_t& rhs) const
+  {
+    bool res = true;
+
+    for (size_t i = 0; i < this->get_seed().size(); i++) {
+      res &= (this->get_seed()[i] == rhs.get_seed()[i]);
+    }
+    res &= (this->get_t_mut() == rhs.get_t_mut());
+
+    return res;
+  }
+
   // Byte serializes the public key.
   inline constexpr void to_bytes(std::span<uint8_t, get_byte_len()> bytes) const
   {
-    // Serialize `seed`
-    std::copy_n(this->seed.begin(), this->seed.size(), bytes.begin());
-
-    // Serialize `t`
-    constexpr size_t coeff_sig_bitcnt = field::Q_BIT_WIDTH - 𝜈t;
-    constexpr uint64_t coeff_sig_bitmask = (1ul << coeff_sig_bitcnt) - 1;
-
-    constexpr size_t buf_max_sig_bitcnt = std::lcm(coeff_sig_bitcnt, std::numeric_limits<uint8_t>::digits);
-    constexpr size_t buf_max_sig_bytes = buf_max_sig_bitcnt / std::numeric_limits<uint8_t>::digits;
-
-    size_t r_idx = 0;
-    size_t pkey_idx = this->seed.size();
-
-    size_t buf_sig_bitcnt = 0;
-    uint64_t buffer = 0;
-
-    static_assert(buf_max_sig_bytes <= sizeof(buffer), "Can't serialize public key into bytes using this method !");
-
-    while (r_idx < k) {
-      for (size_t c_idx = 0; c_idx < raccoon_poly::N; c_idx++) {
-        if (buf_sig_bitcnt == buf_max_sig_bitcnt) {
-          raccoon_utils::to_le_bytes(buffer, bytes.subspan(pkey_idx, buf_max_sig_bytes));
-
-          pkey_idx += buf_max_sig_bytes;
-          buf_sig_bitcnt = 0;
-          buffer = 0;
-        }
-
-        buffer |= (this->t[r_idx][0][c_idx].raw() & coeff_sig_bitmask) << buf_sig_bitcnt;
-        buf_sig_bitcnt += coeff_sig_bitcnt;
-      }
-
-      r_idx++;
-    }
-
-    if (buf_sig_bitcnt == buf_max_sig_bitcnt) {
-      raccoon_utils::to_le_bytes(buffer, bytes.subspan(pkey_idx, buf_max_sig_bytes));
-
-      pkey_idx += buf_max_sig_bytes;
-      buf_sig_bitcnt = 0;
-      buffer = 0;
-    }
+    raccoon_serialization::encode_public_key<𝜅, k, 𝜈t>(this->seed, this->t, bytes);
   }
 
   // Given a byte serialized public key, this routine helps in deserializing it, producing components (seed, t).
   static inline constexpr pkey_t from_bytes(std::span<const uint8_t, get_byte_len()> bytes)
   {
     pkey_t<𝜅, k, 𝜈t> pkey{};
-
-    // Deserialize `seed`
-    std::copy_n(bytes.begin(), pkey.seed.size(), pkey.seed.begin());
-
-    // Deserialize `t`
-    constexpr size_t coeff_sig_bitcnt = field::Q_BIT_WIDTH - 𝜈t;
-    constexpr uint64_t coeff_sig_bitmask = (1ul << coeff_sig_bitcnt) - 1;
-
-    constexpr size_t buf_max_sig_bitcnt = std::lcm(coeff_sig_bitcnt, std::numeric_limits<uint8_t>::digits);
-    constexpr size_t buf_max_sig_bytes = buf_max_sig_bitcnt / std::numeric_limits<uint8_t>::digits;
-    constexpr size_t dec_coeffs_per_round = buf_max_sig_bitcnt / coeff_sig_bitcnt;
-
-    size_t t_idx = 0;
-    size_t pkey_idx = pkey.seed.size();
-
-    static_assert(buf_max_sig_bitcnt <= std::numeric_limits<uint64_t>::digits, "Can't deserialize public key from bytes using this method !");
-
-    while (pkey_idx < bytes.size()) {
-      auto buffer = raccoon_utils::from_le_bytes<uint64_t>(bytes.subspan(pkey_idx, buf_max_sig_bytes));
-      pkey_idx += buf_max_sig_bytes;
-
-      for (size_t i = 0; i < dec_coeffs_per_round; i++) {
-        const size_t ridx = t_idx >> raccoon_poly::LOG2N;
-        const size_t cidx = t_idx & (raccoon_poly::N - 1);
-
-        pkey.t[ridx][0][cidx] = field::zq_t(buffer & coeff_sig_bitmask);
-        buffer >>= coeff_sig_bitcnt;
-
-        t_idx++;
-      }
-    }
-
+    raccoon_serialization::decode_public_key<𝜅, k, 𝜈t>(bytes, pkey.get_seed_mut(), pkey.get_t_mut());
     return pkey;
   }
 };
