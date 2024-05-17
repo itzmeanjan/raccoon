951eeda1cbcfd756bc18e8c0682623fd89fc9270
Anjan Roy <hello@itzmeanjan.in>
2024-05-17 20:50:12 +0400

    refactor tests for serialization and deserialization of public and secret keys to use new data types
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- tests/test_serialization.cpp
+++ tests/test_serialization.cpp
@@ -1,5 +1,3 @@
-#include "polynomial.hpp"
-#include "prng.hpp"
 #include "serialization.hpp"
 #include <gtest/gtest.h>
 
@@ -10,12 +8,12 @@ static void
 test_encode_decode_public_key()
 {
   std::array<uint8_t, 𝜅 / 8> exp_seed{};
-  std::array<polynomial::polynomial_t, k> exp_t{};
+  raccoon_poly_vec::poly_vec_t<k, 1> exp_t{};
 
-  std::array<uint8_t, raccoon_utils::get_pkey_byte_len<𝜅, k, polynomial::N, 𝜈t>()> pkey{};
+  std::array<uint8_t, raccoon_utils::get_pkey_byte_len<𝜅, k, raccoon_poly::N, 𝜈t>()> pkey{};
 
   std::array<uint8_t, 𝜅 / 8> comp_seed{};
-  std::array<polynomial::polynomial_t, k> comp_t{};
+  raccoon_poly_vec::poly_vec_t<k, 1> comp_t{};
 
   constexpr uint64_t coeff_mask = (1ul << (field::Q_BIT_WIDTH - 𝜈t)) - 1;
 
@@ -23,14 +21,14 @@ test_encode_decode_public_key()
   prng::prng_t prng;
 
   prng.read(exp_seed);
-  for (size_t i = 0; i < exp_t.size(); i++) {
-    for (size_t j = 0; j < polynomial::N; j++) {
-      exp_t[i][j] = field::zq_t::random(prng).raw() & coeff_mask;
+  for (size_t i = 0; i < exp_t.num_rows(); i++) {
+    for (size_t j = 0; j < raccoon_poly::N; j++) {
+      exp_t[i][0][j] = field::zq_t::random(prng).raw() & coeff_mask;
     }
   }
 
-  serialization::encode_public_key<𝜅, k, 𝜈t>(exp_seed, exp_t, pkey);
-  serialization::decode_public_key<𝜅, k, 𝜈t>(pkey, comp_seed, comp_t);
+  raccoon_serialization::encode_public_key<𝜅, k, 𝜈t>(exp_seed, exp_t, pkey);
+  raccoon_serialization::decode_public_key<𝜅, k, 𝜈t>(pkey, comp_seed, comp_t);
 
   EXPECT_EQ(exp_seed, comp_seed);
   EXPECT_EQ(exp_t, comp_t);
@@ -47,25 +45,24 @@ template<size_t 𝜅, size_t l>
 static void
 test_unmasked_secret_key_vector_compression_decompression()
 {
-  // Because secret key vector is unmasked
+  // Because we can only test equality of vector `s`, for unmasked case
   constexpr size_t d = 1;
 
-  std::array<polynomial::polynomial_t, l * d> exp_s{};
-  std::array<uint8_t, ((d - 1) * 𝜅 + l * polynomial::N * field::Q_BIT_WIDTH) / 8> s_c{};
-  std::array<polynomial::polynomial_t, l * d> com_s{};
+  raccoon_poly_vec::poly_vec_t<l, d> exp_s{};
+  std::array<uint8_t, ((d - 1) * 𝜅 + l * raccoon_poly::N * field::Q_BIT_WIDTH) / 8> s_c{};
 
   prng::prng_t prng;
 
-  for (size_t i = 0; i < exp_s.size(); i++) {
-    for (size_t j = 0; j < exp_s[i].size(); j++) {
-      exp_s[i][j] = field::zq_t::random(prng);
+  for (size_t i = 0; i < exp_s.num_rows(); i++) {
+    for (size_t j = 0; j < exp_s[i].num_shares(); j++) {
+      exp_s[i][j] = raccoon_poly::poly_t::random(prng);
     }
   }
 
-  serialization::mask_compress<𝜅, l, d>(exp_s, s_c, prng);
-  serialization::mask_decompress<𝜅, l, d>(s_c, com_s);
+  raccoon_serialization::mask_compress<𝜅, l, d>(exp_s, s_c, prng);
+  const auto comp_s = raccoon_serialization::mask_decompress<𝜅, l, d>(s_c);
 
-  EXPECT_EQ(exp_s, com_s);
+  EXPECT_EQ(exp_s, comp_s);
 }
 
 TEST(RaccoonSign, UnmaskedSecretKeyVectorCompressionAndDecompression)
@@ -80,8 +77,8 @@ static void
 test_encode_decode_signature_all_zeros()
 {
   std::array<uint8_t, (2 * 𝜅) / std::numeric_limits<uint8_t>::digits> orig_c_hash{};
-  std::array<int64_t, k * polynomial::N> orig_h{};
-  std::array<int64_t, l * polynomial::N> orig_z{};
+  std::array<int64_t, k * raccoon_poly::N> orig_h{};
+  std::array<int64_t, l * raccoon_poly::N> orig_z{};
   std::array<uint8_t, sig_byte_len> sig{};
   std::array<uint8_t, orig_c_hash.size()> comp_c_hash{};
   std::array<int64_t, orig_h.size()> comp_h{};
@@ -102,8 +99,8 @@ test_encode_decode_signature_all_zeros()
   std::fill(comp_h.begin(), comp_h.end(), std::numeric_limits<int64_t>::max());
   std::fill(comp_z.begin(), comp_z.end(), std::numeric_limits<int64_t>::max());
 
-  const auto is_encoded = serialization::encode_sig<k, l, 𝜅, sig_byte_len>(orig_c_hash, orig_h, orig_z, sig);
-  const auto is_decoded = serialization::decode_sig<k, l, 𝜅, sig_byte_len>(sig, comp_c_hash, comp_h, comp_z);
+  const auto is_encoded = raccoon_serialization::encode_sig<k, l, 𝜅, sig_byte_len>(orig_c_hash, orig_h, orig_z, sig);
+  const auto is_decoded = raccoon_serialization::decode_sig<k, l, 𝜅, sig_byte_len>(sig, comp_c_hash, comp_h, comp_z);
 
   EXPECT_TRUE(is_encoded);
   EXPECT_TRUE(is_decoded);
