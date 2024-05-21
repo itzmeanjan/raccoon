6ff290381bcd341e6308deb4fd0bab77b58754e4
Anjan Roy <hello@itzmeanjan.in>
2024-05-21 20:28:07 +0400

    implement signature verification algorithm under public key data type
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/public_key.hpp
+++ include/public_key.hpp
@@ -1,7 +1,14 @@
 #pragma once
+#include "challenge.hpp"
+#include "field.hpp"
+#include "poly_mat.hpp"
 #include "poly_vec.hpp"
 #include "serialization.hpp"
+#include "shake256.hpp"
+#include "signature.hpp"
 #include "utils.hpp"
+#include <array>
+#include <cstdint>
 
 namespace raccoon_pkey {
 
@@ -43,6 +50,85 @@ public:
     res &= (this->get_t_mut() == rhs.get_t_mut());
 
     return res;
+  }
+
+  // Given a byte serialized Raccon signature and corresponding message (which was signed by the owner of the secret key, which is linked to this public key),
+  // this routine verifies the validity of the signature, returning boolean truth value in case of success, else returning false. This is an implementation of
+  // the algorithm 3 of the specification.
+  template<size_t l, size_t 𝜈w, size_t 𝜔, size_t sig_byte_len, uint64_t Binf, uint64_t B22>
+  inline constexpr bool verify(std::span<const uint8_t> msg, std::span<const uint8_t, sig_byte_len> sig)
+  {
+    // Step 1: Attempt to decode signature into its components
+    auto sig_opt = raccoon_sig::sig_t<𝜅, k, l, 𝜈w, sig_byte_len>::from_bytes(sig);
+    const bool is_decoded = sig_opt.has_value();
+    if (!is_decoded) {
+      // Signature can't be deserialized back into its components
+      return false;
+    }
+
+    auto sig_obj = sig_opt.value();
+
+    // Step 2: Perform norms check
+    const bool is_under_bounds = sig_obj.template check_bounds<Binf, B22>();
+    if (!is_under_bounds) {
+      // Signature is failing norms check
+      return false;
+    }
+
+    std::array<uint8_t, raccoon_utils::get_pkey_byte_len<𝜅, k, raccoon_poly::N, 𝜈t>()> pk_bytes{};
+    this->to_bytes(pk_bytes);
+
+    // Step 3: Bind public key with message
+    std::array<uint8_t, (2 * 𝜅) / std::numeric_limits<uint8_t>::digits> 𝜇{};
+    shake256::shake256_t hasher{};
+
+    hasher.absorb(pk_bytes);
+    hasher.finalize();
+    hasher.squeeze(𝜇);
+
+    hasher.reset();
+
+    hasher.absorb(𝜇);
+    hasher.absorb(msg);
+    hasher.finalize();
+    hasher.squeeze(𝜇);
+
+    // Step 4: Generate uniform matrix A
+    const auto A = raccoon_poly_mat::poly_mat_t<k, l>::template expandA<k, l, 𝜅>(this->seed);
+
+    // Extract components of signature
+    auto c_hash = sig_obj.get_c_hash();
+    auto h = sig_obj.get_h();
+    auto z = sig_obj.get_z();
+    z.ntt();
+
+    // Step 5: Compute challenge polynomial
+    auto c_poly = raccoon_poly::poly_t::chal_poly<𝜅, 𝜔>(c_hash);
+    c_poly.ntt();
+
+    auto t = this->t;
+    t = t << 𝜈t;
+    t.ntt();
+
+    // Step 6: Recompute noisy LWE commitment vector y
+    auto y = A * z - t * c_poly;
+    y.intt();
+
+    // Step 7: Adjust LWE commitment vector with hint vector, reduced small moduli `q >> 𝜈w`
+    y.template rounding_shr<𝜈w>();
+    auto w = y.template add_mod<(field::Q >> 𝜈w)>(h);
+
+    // Step 8: Recompute challenge hash
+    std::array<uint8_t, c_hash.size()> c_hash_prime{};
+    raccoon_challenge::chal_hash<k, 𝜅>(w, 𝜇, c_hash_prime);
+
+    using c_hash_t = std::span<const uint8_t, c_hash.size()>;
+
+    // Step 9: Check equality of commitment
+    const auto is_equal = raccoon_utils::ct_eq_byte_array(c_hash_t(c_hash), c_hash_t(c_hash_prime));
+    const auto is_verified = static_cast<bool>(is_equal >> (std::numeric_limits<decltype(is_equal)>::digits - 1));
+
+    return is_verified;
   }
 
   // Byte serializes the public key.
