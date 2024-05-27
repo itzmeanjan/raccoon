125e8600d6b3fb801a05bfc703740a53cd52116a
Anjan Roy <hello@itzmeanjan.in>
2024-05-27 19:32:43 +0400

    Benchmark Raccoon128 signing algorithm for various masking settings
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- benchmarks/raccoon128.cpp
+++ benchmarks/raccoon128.cpp
@@ -25,15 +25,46 @@ bench_raccoon128_keygen(benchmark::State& state)
   state.SetItemsProcessed(state.iterations());
 }
 
+template<size_t d>
+static void
+bench_raccoon128_sign(benchmark::State& state)
+{
+  constexpr size_t fixed_msg_byte_len = 32;
+
+  std::array<uint8_t, 16> seed{};
+  std::array<uint8_t, raccoon128::SIG_BYTE_LEN> sig_bytes{};
+  std::vector<uint8_t> msg(fixed_msg_byte_len, 0);
+
+  prng::prng_t prng{};
+  prng.read(seed);
+  prng.read(msg);
+
+  auto skey = raccoon128::raccoon128_skey_t<d>::generate(seed);
+
+  for (auto _ : state) {
+    skey.sign(msg, sig_bytes);
+
+    benchmark::DoNotOptimize(msg);
+    benchmark::DoNotOptimize(sig_bytes);
+    benchmark::DoNotOptimize(skey);
+    benchmark::ClobberMemory();
+  }
+
+  state.SetItemsProcessed(state.iterations());
+}
+
+// clang-format off
 BENCHMARK(bench_raccoon128_keygen<1>)->Name("raccoon128/keygen/unmasked")->ComputeStatistics("min", compute_min)->ComputeStatistics("max", compute_max);
-BENCHMARK(bench_raccoon128_keygen<2>)->Name("raccoon128/keygen/masked(order 1)")->ComputeStatistics("min", compute_min)->ComputeStatistics("max", compute_max);
-BENCHMARK(bench_raccoon128_keygen<4>)->Name("raccoon128/keygen/masked(order 3)")->ComputeStatistics("min", compute_min)->ComputeStatistics("max", compute_max);
-BENCHMARK(bench_raccoon128_keygen<8>)->Name("raccoon128/keygen/masked(order 7)")->ComputeStatistics("min", compute_min)->ComputeStatistics("max", compute_max);
-BENCHMARK(bench_raccoon128_keygen<16>)
-  ->Name("raccoon128/keygen/masked(order 15)")
-  ->ComputeStatistics("min", compute_min)
-  ->ComputeStatistics("max", compute_max);
-BENCHMARK(bench_raccoon128_keygen<32>)
-  ->Name("raccoon128/keygen/masked(order 31)")
-  ->ComputeStatistics("min", compute_min)
-  ->ComputeStatistics("max", compute_max);
+BENCHMARK(bench_raccoon128_keygen<2>)->Name("raccoon128/keygen/masked/order 1")->ComputeStatistics("min", compute_min)->ComputeStatistics("max", compute_max);
+BENCHMARK(bench_raccoon128_keygen<4>)->Name("raccoon128/keygen/masked/order 3")->ComputeStatistics("min", compute_min)->ComputeStatistics("max", compute_max);
+BENCHMARK(bench_raccoon128_keygen<8>)->Name("raccoon128/keygen/masked/order 7")->ComputeStatistics("min", compute_min)->ComputeStatistics("max", compute_max);
+BENCHMARK(bench_raccoon128_keygen<16>)->Name("raccoon128/keygen/masked/order 15")->ComputeStatistics("min", compute_min)->ComputeStatistics("max", compute_max);
+BENCHMARK(bench_raccoon128_keygen<32>)->Name("raccoon128/keygen/masked/order 31")->ComputeStatistics("min", compute_min)->ComputeStatistics("max", compute_max);
+
+BENCHMARK(bench_raccoon128_sign<1>)->Name("raccoon128/sign/unmasked")->ComputeStatistics("min", compute_min)->ComputeStatistics("max", compute_max);
+BENCHMARK(bench_raccoon128_sign<2>)->Name("raccoon128/sign/masked/order 1")->ComputeStatistics("min", compute_min)->ComputeStatistics("max", compute_max);
+BENCHMARK(bench_raccoon128_sign<4>)->Name("raccoon128/sign/masked/order 3")->ComputeStatistics("min", compute_min)->ComputeStatistics("max", compute_max);
+BENCHMARK(bench_raccoon128_sign<8>)->Name("raccoon128/sign/masked/order 7")->ComputeStatistics("min", compute_min)->ComputeStatistics("max", compute_max);
+BENCHMARK(bench_raccoon128_sign<16>)->Name("raccoon128/sign/masked/order 15")->ComputeStatistics("min", compute_min)->ComputeStatistics("max", compute_max);
+BENCHMARK(bench_raccoon128_sign<32>)->Name("raccoon128/sign/masked/order 31")->ComputeStatistics("min", compute_min)->ComputeStatistics("max", compute_max);
+// clang-format on
