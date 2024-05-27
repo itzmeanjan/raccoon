db393d38cb472ecf8458e1b11841bef9dc49063b
Anjan Roy <hello@itzmeanjan.in>
2024-05-27 18:52:34 +0400

    benchmark Raccoon128 unmasked key generation algorithm
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

---
+++ benchmarks/raccoon128.cpp
@@ -0,0 +1,28 @@
+#include "raccoon/raccoon128.hpp"
+#include "bench_common.hpp"
+#include <benchmark/benchmark.h>
+
+template<size_t d>
+static void
+bench_raccoon128_keygen(benchmark::State& state)
+{
+  std::array<uint8_t, 16> seed{};
+  std::array<uint8_t, raccoon128::raccoon128_skey_t<d>::get_byte_len()> sk_bytes{};
+
+  prng::prng_t prng{};
+  prng.read(seed);
+
+  for (auto _ : state) {
+    auto skey = raccoon128::raccoon128_skey_t<d>::generate(seed);
+    skey.as_bytes(sk_bytes);
+
+    benchmark::DoNotOptimize(seed);
+    benchmark::DoNotOptimize(skey);
+    benchmark::DoNotOptimize(sk_bytes);
+    benchmark::ClobberMemory();
+  }
+
+  state.SetItemsProcessed(state.iterations());
+}
+
+BENCHMARK(bench_raccoon128_keygen<1>)->Name("raccoon128/keygen/unmasked")->ComputeStatistics("min", compute_min)->ComputeStatistics("max", compute_max);
